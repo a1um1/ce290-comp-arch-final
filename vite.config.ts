ebaa49a383a01e00cbda876a5439e4805fed1a7f
katesan945 <katesan945@gmail.com>
2026-04-27 13:34:41 +0700

    feat: update base path in Vite config for final project structure

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ce290-comp-arch/",
+  base: "/ce290-comp-arch-final/",
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
